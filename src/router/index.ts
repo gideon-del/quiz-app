c8635479aaafd742eba9b718093e0649df0578ab
Gideon Chidi <gideonchidi471@gmail.com>
2024-07-26 22:20:55 +0100

    fix: fixing deployment issue

--- src/router/index.ts
+++ src/router/index.ts
@@ -40,7 +40,7 @@ const checkQuizSessionStatus = async (to: RouteLocationNormalizedGeneric) => {
   }
 }
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
